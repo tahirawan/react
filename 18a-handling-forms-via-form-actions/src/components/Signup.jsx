316d4c26367a4e882028da9ad77df1071b22f24b
Tahir Awan <tahirawan01@gmail.com>
2025-04-23 16:58:22 +0100

    18a form actions

---
+++ 18a-handling-forms-via-form-actions/src/components/Signup.jsx
@@ -0,0 +1,184 @@
+import { useActionState } from "react";
+import {hasMinLength, isEmail, isEqualToOtherValue, isNotEmpty} from "../util/validation.js";
+
+function signupAction(prevFormState, formData) {
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const confirmPassword = formData.get('confirm-password');
+    const firstName = formData.get('first-name');
+    const lastName = formData.get('last-name');
+    const role = formData.get('role');
+    const acquisition = formData.getAll('acquisition');
+    const terms = formData.get('terms');
+
+    let errors = [];
+
+    if (!isEmail(email)) {
+        errors.push('Email is not valid');
+    }
+
+    if (!isNotEmpty(password) || !hasMinLength(password, 6)) {
+        errors.push('Password must be at least 6 characters long');
+    }
+
+    if (!isEqualToOtherValue(password, confirmPassword)) {
+        errors.push('Passwords do not match');
+    }
+
+    if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
+        errors.push('First name and last name are required');
+    }
+
+    if (!isNotEmpty(role)) {
+        errors.push('Role is required');
+    }
+
+    if (acquisition.length === 0) {
+        errors.push('You must select at least one acquisition channel');
+    }
+
+    if (!terms) {
+        errors.push('You must accept the terms and conditions');
+    }
+
+    if (errors.length > 0) {
+        console.log('Errors: ', errors);
+        return {
+            errors: errors,
+            enteredValues: {
+                email,
+                password,
+                confirmPassword,
+                firstName,
+                lastName,
+                role,
+                acquisition,
+                terms
+            }
+        };
+    }
+
+    return {
+        errors: null
+    }
+
+    console.log('Email: ', email);
+    console.log(formData);
+}
+
+export default function Signup() {
+    const [formState, formAction] = useActionState(signupAction, { errors: null });
+  return (
+    <form action={formAction}>
+      <h2>Welcome on board!</h2>
+      <p>We just need a little bit of data from you to get you started 🚀</p>
+
+      <div className="control">
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" name="email" defaultValue={formState.enteredValues?.email} />
+      </div>
+
+      <div className="control-row">
+        <div className="control">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password" defaultValue={formState.password?.email}  />
+        </div>
+
+        <div className="control">
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <input
+            id="confirm-password"
+            type="password"
+            name="confirm-password"
+            defaultValue={formState.enteredValues?.confirmPassword}
+          />
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="control-row">
+        <div className="control">
+          <label htmlFor="first-name">First Name</label>
+          <input type="text" id="first-name" name="first-name" defaultValue={formState.enteredValues?.firstName}  />
+        </div>
+
+        <div className="control">
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" id="last-name" name="last-name" defaultValue={formState.enteredValues?.lastName}  />
+        </div>
+      </div>
+
+      <div className="control">
+        <label htmlFor="phone">What best describes your role?</label>
+        <select id="role" name="role" defaultValue={formState.enteredValues?.role} >
+          <option value="student">Student</option>
+          <option value="teacher">Teacher</option>
+          <option value="employee">Employee</option>
+          <option value="founder">Founder</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+
+      <fieldset>
+        <legend>How did you find us?</legend>
+        <div className="control">
+          <input
+            type="checkbox"
+            id="google"
+            name="acquisition"
+            value="google"
+            defaultChecked={formState.enteredValues?.acquisition.includes('google')}
+          />
+          <label htmlFor="google">Google</label>
+        </div>
+
+        <div className="control">
+          <input
+            type="checkbox"
+            id="friend"
+            name="acquisition"
+            value="friend"
+            defaultChecked={formState.enteredValues?.acquisition.includes('friend')}
+          />
+          <label htmlFor="friend">Referred by friend</label>
+        </div>
+
+        <div className="control">
+          <input
+              type="checkbox"
+              id="other"
+              name="acquisition"
+              value="other"
+              defaultChecked={formState.enteredValues?.acquisition.includes('other')}
+          />
+          <label htmlFor="other">Other</label>
+        </div>
+      </fieldset>
+
+      <div className="control">
+        <label htmlFor="terms-and-conditions">
+          <input type="checkbox" id="terms-and-conditions" name="terms" defaultChecked={formState.enteredValues?.terms} />I
+          agree to the terms and conditions
+        </label>
+      </div>
+
+        {formState.errors && (
+            <ul className="error">
+                {
+                    formState.errors.map((error, index) => (
+                    <li key={index}>{error}</li>
+                ))
+                }
+            </ul>
+        )}
+
+      <p className="form-actions">
+        <button type="reset" className="button button-flat">
+          Reset
+        </button>
+        <button className="button">Sign up</button>
+      </p>
+    </form>
+  );
+}
